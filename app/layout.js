5f7a4879b4f1fa9ffa42b68e9741d46dcd003d60
shivani mehta <yournewemail@example.com>
2024-09-18 11:48:21 +0530

    correct in import a font size

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,9 @@
 
 import { Inter } from "next/font/google";
-// import "./ font.css";
+import "./font.css"
 import "./globals.css";
 import Footer from "./components/Footer.js";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}
-      <Footer />
+        <Footer />
       </body>
     </html>
   );
